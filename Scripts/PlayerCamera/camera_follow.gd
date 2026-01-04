72890b44f2401ef9cc7223f2f3f19132e08bd803
RHerrera30 <152225484+RHerrera30@users.noreply.github.com>
2026-01-03 17:46:27 -0800

    Added screenshot function

--- Scripts/PlayerCamera/camera_follow.gd
+++ Scripts/PlayerCamera/camera_follow.gd
@@ -1,7 +1,33 @@
 extends Camera3D
 
+#@export var camera_state: Node
+
 @export var spring_arm: Node3D
 @export var lerp_power: float = 1.0
+
+#screenshot function
+var screenShotCount = 1
+
+func _ready():
+	var dir = DirAccess.open("user://")
+	dir.make_dir("screenshots")
+	dir = DirAccess.open("user://screenshots")
+	
+	for n in dir.get_files():
+		screenShotCount += 1
+
+func _input(event):
+	if event.is_action_pressed("screenshot"):
+		screenshot()
+
+func screenshot():
+	await RenderingServer.frame_post_draw
+	
+	var viewport = get_viewport()
+	var img = viewport.get_texture().get_image()
+	img.save_png("user://screenshots/ss"+str(screenShotCount)+".png")
+	screenShotCount += 1
+	print("Screenshot taken")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
